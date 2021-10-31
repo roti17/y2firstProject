18410abcae8c96f7506ab98b66a5af89a72073c6
roti17 <93337478+roti17@users.noreply.github.com>
2021-10-31 13:40:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -43,9 +43,6 @@ def delete(id):
   return redirect(url_for('shop')) 
    
 if __name__ == "__main__":  # Makes sure this is the main process
-	app.run( # Starts the site
-		host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
-		port=random.randint(2000, 9000),  # Randomly select the port the machine hosts on.
-    debug=True
-	)
+	app.run( host = 'loaclhost', port = 5000, debug=true)
+	
 
